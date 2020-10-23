64e8d490ad2519a3e94144b21c2394b83178973e
duccyf <duccyf@gmail.com>
2020-10-23 08:39:36 +0700

    add redux logic

--- screens/Home/data/logic.ts
+++ screens/Home/data/logic.ts
@@ -1,35 +1,36 @@
-// import { createLogic } from 'redux-logic';
-// import * as types from './actionTypes';
+import { createLogic } from 'redux-logic';
+import * as types from './actionTypes';
 
-// export const homeLogic = createLogic({
-//   type: types.HOME_SEARCH_SUCCESS, // filter for actions of this type
-//   cancelType: types.HOME_SEARCH_CANCELLED, // cancel if action is dispatched
-//   // throttle: 2000,
-//   latest: true, // only provide the latest if fired many times
-//   processOptions: {
-//     dispatchReturn: false, // dispatch from resolved/rejected promise
-//     failType: types.HOME_SEARCH_FAILED, // use action type for errors
-//   },
-//   async process({ getState, action }, dispatch, done) {
-//     // const clipId = action.payload;
-//     // const clipDetail = await getVideo(clipId, true);
-//     // if (!clipDetail.error) {
-//     //   if (clipId.startsWith('tv_')) {
-//     //     const video = clipDetail.video;
-//     //     const match = /external\/\d+/.exec(video.file);
-//     //     if (match) {
-//     //       const id = match[0].replace('external/', '');
-//     //       clipDetail.video.uri = `https://player.vimeo.com/video/${id}`;
-//     //     }
-//     //   }
-//     dispatch({ type: types.HOME_SEARCH_SUCCESS, payload: { isLoading: false } });
-//     // }
-//     done();
-//   },
-// });
+export const homeLogic = createLogic({
+  type: types.HOME_SEARCH_ACTION, // filter for actions of this type
+  cancelType: types.HOME_SEARCH_CANCELLED, // cancel if action is dispatched
+  // throttle: 2000,
+  latest: true, // only provide the latest if fired many times
+  processOptions: {
+    dispatchReturn: false, // dispatch from resolved/rejected promise
+    failType: types.HOME_SEARCH_FAILED, // use action type for errors
+  },
+  async process({}, dispatch, done) {
+    console.log('logic')
+    // const clipId = action.payload;
+    // const clipDetail = await getVideo(clipId, true);
+    // if (!clipDetail.error) {
+    //   if (clipId.startsWith('tv_')) {
+    //     const video = clipDetail.video;
+    //     const match = /external\/\d+/.exec(video.file);
+    //     if (match) {
+    //       const id = match[0].replace('external/', '');
+    //       clipDetail.video.uri = `https://player.vimeo.com/video/${id}`;
+    //     }
+    //   }
+    dispatch({ type: types.HOME_SEARCH_SUCCESS, payload: { isLoading: true } });
+    // }
+    done();
+  },
+});
 
 
 
 export default [
-  // homeLogic,
+  homeLogic,
 ];
--- store/index.ts
+++ store/index.ts
@@ -1,19 +1,47 @@
-import {createStore, AnyAction, applyMiddleware} from 'redux';
-import {MakeStore, createWrapper, HYDRATE} from 'next-redux-wrapper';
+import { createStore, AnyAction, compose, applyMiddleware } from 'redux';
+import { MakeStore, createWrapper, HYDRATE } from 'next-redux-wrapper';
 import { createLogicMiddleware } from 'redux-logic';
 import rootReducer from './rootReducer';
 import arrLogic from './rootLogic';
 
 const deps = {};
+let store: any = null
 
-const logicMiddleware = createLogicMiddleware(arrLogic, deps);
+const initStore = (initialState = {}): any => {
+  const logicMiddleware = createLogicMiddleware(arrLogic, deps);
+  const middlewares = [
+    logicMiddleware,
+  ];
+  const _store = createStore(rootReducer, initialState, compose(applyMiddleware(...middlewares)));
+  return _store
+}
 
-const middleware = applyMiddleware(
-  logicMiddleware
-);
+export const initializeStore = (preloadedState: any) => {
+  let _store = store ?? initStore(preloadedState)
+
+  // After navigating to a page with an initial Redux state, merge that state
+  // with the current state in the store, and create a new store
+  if (preloadedState && store) {
+    _store = initStore({
+      ...store.getState(),
+      ...preloadedState,
+    })
+    // Reset the current store
+    store = undefined
+  }
+
+  // For SSG and SSR always create a new store
+  if (typeof window === 'undefined') return _store
+  // Create the store once in the client
+  if (!store) store = _store
+
+  return _store
+}
+
+
 
 // create a makeStore function
-const makeStore = () => createStore(rootReducer, {}, middleware);
+const makeStore = () => initializeStore({});
 
 // export an assembled wrapper
-export const wrapper = createWrapper(makeStore, {debug: true});
+export const wrapper = createWrapper(makeStore, { debug: true });
